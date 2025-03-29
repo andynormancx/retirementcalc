ee0e895336c137c6502ae393d739d366957b5410
Andy Norman <andy@norman.cx>
2025-03-29 16:48:25 +0000

    Moved the load/save to the top of the page, made the scenario load when you use the select, moved all the lists to the generic code

--- ui.js
+++ ui.js
@@ -11,10 +11,12 @@ export function setupUI(defaultState) {
 
     setupList('otherAnnualIncomeContainer', 'otherAnnualIncomeTemplate',
         model.otherAnnualIncomes, createEmptyListItemOtherAnnualIncome)
-    
-    //setupOtherAnnualIncomes()
-    setupLumpSums()
-    setupGrossIncomeRates()
+
+    setupList('lumpSumsContainer', 'lumpSumTemplate',
+        model.lumpSums, createEmptyListItemLumpSums)
+
+    setupList('grossIncomeRatesContainer', 'grossIncomeRateTemplate',
+        model.grossExpenditureRates, createEmptyListItemGrossExpenditureRates)            
 
     bindInputs()
     updateProjection()
@@ -152,138 +154,21 @@ function createEmptyListItemOtherAnnualIncome() {
     }    
 }
 
-function setupOtherAnnualIncomes() {
-    document.getElementById('otherAnnualIncomeContainer').innerHTML = '';
-
-    model.otherAnnualIncomes.forEach((income, index) => {
-        addOtherAnnualIncome(income.id)
-    })
-
-    const btn = document.getElementById('otherAnnualIncomeAddBtn');
-    btn.addEventListener('click', function () {
-        addNewOtherAnnualIncome()
-    })
-}
-
-function addOtherAnnualIncome(index) {
-    const container = document.getElementById('otherAnnualIncomeContainer');
-    const template = document.getElementById('otherAnnualIncomeTemplate');
-    const clone = template.content.cloneNode(true);
-  
-    const removeBtn = clone.querySelector('.remove-btn');
-  
-    const incomeElement = clone.firstElementChild;
-
-    const reactiveIncome = model.otherAnnualIncomes[index];
-    bindChildElements(incomeElement, reactiveIncome, false);
-
-    removeBtn.addEventListener('click', function () {
-        model.otherAnnualIncomes.splice(index, 1);
-        // Remove from DOM
-        incomeElement.remove();
-        updateProjection();
-    });
-  
-    container.appendChild(clone);
-}
-
-function addNewOtherAnnualIncome(index) {
-    model.otherAnnualIncomes.push({
+function createEmptyListItemLumpSums() {
+    return {
         id: uuidv4(),
         age: model.firstStatePensionAge,
         amount: 0
-    })
-    addOtherAnnualIncome(model.otherAnnualIncomes.length - 1)
+    }
 }
 
-function setupLumpSums() {
-    document.getElementById('lumpSumsContainer').innerHTML = '';
-
-    model.lumpSums.forEach((_, index) => {
-        addLumpSum(index)
-    })
-
-    const btn = document.getElementById('lumpSumsAddBtn');
-    btn.addEventListener('click', function () {
-        addNewLumpSum()
-    })
-}
-
-function addLumpSum(index) {
-    const container = document.getElementById('lumpSumsContainer');
-    const template = document.getElementById('lumpSumTemplate');
-    const clone = template.content.cloneNode(true);
-  
-    const removeBtn = clone.querySelector('.remove-btn');
-  
-    const incomeElement = clone.firstElementChild;
-
-    const reactiveIncome = model.lumpSums[index];
-    bindChildElements(incomeElement, reactiveIncome, false);
-
-    removeBtn.addEventListener('click', function () {
-        model.lumpSums.splice(index, 1);
-        // Remove from DOM
-        incomeElement.remove();
-        updateProjection();
-    });
-  
-    container.appendChild(clone);
-}
-
-function addNewLumpSum() {
-    model.lumpSums.push({
+function createEmptyListItemGrossExpenditureRates() {
+    return {
         id: uuidv4(),
         age: model.firstStatePensionAge,
         amount: 0
-    })
-    addLumpSum(model.lumpSums.length - 1)
+    }
 }
-
-function setupGrossIncomeRates() {
-    document.getElementById('grossIncomeRatesContainer').innerHTML = '';
-
-    model.grossExpenditureRates.forEach((_, index) => {
-        addGrossIncomeRate(index)
-    })
-
-    const btn = document.getElementById('grossIncomeRatesAddBtn');
-    btn.addEventListener('click', function () {
-        addNewGrossIncomeRate()
-    })
-}
-
-function addGrossIncomeRate(index) {
-    const container = document.getElementById('grossIncomeRatesContainer');
-    const template = document.getElementById('grossIncomeRateTemplate');
-    const clone = template.content.cloneNode(true);
-  
-    const removeBtn = clone.querySelector('.remove-btn');
-  
-    const incomeElement = clone.firstElementChild;
-
-    const reactiveIncome = model.grossExpenditureRates[index];
-    bindChildElements(incomeElement, reactiveIncome, false);
-
-    removeBtn.addEventListener('click', function () {
-        model.grossExpenditureRates.splice(index, 1);
-        // Remove from DOM
-        incomeElement.remove();
-        updateProjection();
-    });
-  
-    container.appendChild(clone);
-}
-
-function addNewGrossIncomeRate() {
-    model.grossExpenditureRates.push({
-        id: uuidv4(),
-        age: model.firstStatePensionAge,
-        amount: 0
-    })
-    addGrossIncomeRate(model.grossExpenditureRates.length - 1)
-}
-
 
 function refreshSavedModelsDropdown() {
     const dropdown = document.getElementById('savedModelsDropdown');
@@ -341,9 +226,11 @@ function handleLoad() {
 window.addEventListener('DOMContentLoaded', () => {
     const saveBtn = document.getElementById('saveModelBtn');
     const loadBtn = document.getElementById('loadModelBtn');
+    const savedModelsDropdown = document.getElementById('savedModelsDropdown');
 
     if (saveBtn) saveBtn.addEventListener('click', handleSave);
     if (loadBtn) loadBtn.addEventListener('click', handleLoad);
+    if (savedModelsDropdown) savedModelsDropdown.addEventListener('change', handleLoad)
 
     refreshSavedModelsDropdown();
 });
