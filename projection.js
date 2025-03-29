2656971955771315a8cef187142e2ae3a40a6bf5
Andy Norman <andy@norman.cx>
2025-03-29 16:54:07 +0000

    Delete dead code

--- projection.js
+++ projection.js
@@ -10,31 +10,3 @@ window.addEventListener('load', () => {
 
     setupUI(defaultState);
 });
-
-
-window.onload = function () {
-    return
-
-    const currentYear = new Date().getFullYear();
-    const currentAge = currentYear - parseInt(document.getElementById('firstBirthYear').value);
-    addGrossIncomeRate(currentAge, 6000);
-    addGrossIncomeRate(65, 5000);
-    addGrossIncomeRate(75, 3500);
-    addLumpSum(currentAge, -90000);
-    addLumpSum(65, 100000);
-    addOtherAnnualIncome(54, 1000, false);
-    const inputIds = [
-        'initialBalance', 'annualReturn', 'inflationRate', 'grossIncome',
-        'firstBirthYear', 'secondBirthYear', 'firstStatePensionAge',
-        'secondStatePensionAge', 'initialStatePension'
-    ];
-    
-    inputIds.forEach(id => {
-        const el = document.getElementById(id);
-        if (el) {
-            el.addEventListener('input', generateProjection);
-        }
-    });
-
-    generateProjection();
-};
